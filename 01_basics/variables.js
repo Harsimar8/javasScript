92527db05f43a6007c4a8f9a339a8746bb06f83c
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-17 16:35:27 +0000

    variable

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 144532
+let accountEmail = "jia23@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId =1   ,  only const cant be changed
+
+accountEmail = "jia33@gmail.com"
+accountPassword ="89890"
+accountCity =" Bengaluru"
+let accountState;
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
+
