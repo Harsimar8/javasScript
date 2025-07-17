cc2b51515123c183f9353113d076ec0a67ac16bd
Harsimar8 <156052380+Harsimar8@users.noreply.github.com>
2025-07-17 16:33:42 +0000

    datatypes & operation

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"
+
+let score = 33
+
+// console.log(typeof score);
+// let min = "33abc"
+// console.log(typeof min);
+// let ss = Number(min);
+// console.log(typeof ss);
+// console.log(ss);
+
+// let s  = true
+// console.log(typeof s);
+
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+let isLoggedInone = ""
+let booleanLoggedInone = Boolean(isLoggedIn)
+console.log(booleanLoggedInone);
+
+let isLoggedIntwo = "Jia"
+let booleanLoggedIntwo = Boolean(isLoggedIntwo)
+console.log(booleanLoggedIntwo);
+
+let some =33
+let stringN = String(some)
+console.log( typeof stringN);
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/operations.js
@@ -0,0 +1,25 @@
+let value= 3;
+let negVal = -value
+console.log(negVal);
+
+
+let str1 = "hi"
+let str2 = "jia"
+
+console.log( str1 + str2);
+console.log("1" + 2);
+console.log("1" + "2");
+console.log(2 + "1");
+console.log(1 + 2 + "2");
+console.log("2" + 1 + 1);
+console.log(+true);   // as true represents 1
+// console.log(true+);  // gives an error , true + is not anything
+
+console.log(+"");
+
+
+
+
+
+
+
